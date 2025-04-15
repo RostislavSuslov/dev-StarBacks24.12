f45b271263130f0802e66a6d3e5da59080e57693
RostSuslov <suslovros@gmail.com>
2025-04-16 01:42:02 +0300

    feat: creat videoplayer

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,9 +1,24 @@
-const burger = document.querySelector('.btn-burger');
+const burger = document.querySelector('.btn-burger')
 const mobileContainer = document.querySelector('.mobile-container')
 
-burger.addEventListener('click', ()=> {
-    burger.classList.toggle('active')
+function toggleMobileContainer() {
     mobileContainer.classList.toggle('show')
+    burger.classList.toggle('active')
+}
+
+burger.addEventListener('click', toggleMobileContainer)
+
+const label = document.querySelectorAll('.label')
+const btn = document.querySelectorAll('.btn')
+
+btn.forEach(item => {
+    item.addEventListener('mousedown', function() {
+        item.classList.add('hovered')
+    })
+
+    item.addEventListener('mouseup', function() {
+        item.classList.remove('hovered')
+    })
 })
 
-
+ 
--- dev/assets/js/modal.js
+++ dev/assets/js/modal.js
@@ -1,17 +1,60 @@
-const triger = document.querySelector('#triger')
-const modal = document.querySelector('.modal')
-const close = document.querySelector('.btn-close')
 const body = document.querySelector('body')
+const trigers = document.querySelectorAll('[data-modal]')
+const closeTrigers = document.querySelectorAll('[data-close-modal]')
+ 
+function closeModal(element, event) {
+    event.preventDefault()
+    element.closest('.modal').classList.remove('d-flex')
+    body.classList.remove('overflow-hidden')
+}
 
-function showModal() {
+function showModal(modal) {
     modal.classList.add('d-flex')
     body.classList.add('overflow-hidden')
 }
 
-function closeModal() {
-    modal.classList.remove('d-flex')
-    body.classList.remove('overflow-hidden')
-}
+trigers.forEach(
+    function(element) {
+        const modalId = element.getAttribute('data-modal')
+        element.addEventListener('click', function(event) {
+            event.preventDefault()
+            const modal = document.getElementById(modalId)
+            showModal(modal)
+        })
+    }
+)
 
-triger.addEventListener('click', showModal)
-close.addEventListener('click', closeModal)
+closeTrigers.forEach(function(element) {
+    element.addEventListener('click', function(event) {
+        closeModal(element, event)
+    })
+})
+
+/*
+
+EXAMPLE:
+
+Button trigger modal
+<a href="#" data-modal="feedback">feedback</a>
+
+Modal 
+<div id="feedback" class="modal">
+    <a href="#" class="btn-close" data-close-modal></a>
+    <h2>feedback</h2>
+</div> 
+.
+.
+.
+.
+.
+.
+TRIGER
+<a href="#" data-modal="qwerty">qwerty</a>
+
+MODAL WINDOW
+<div id="qwerty" class="modal">
+    <a href="#" class="btn-close" data-close-modal></a>
+    <h2>qwerty</h2>
+</div> 
+
+*/
