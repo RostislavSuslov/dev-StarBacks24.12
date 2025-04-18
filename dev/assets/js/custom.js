a880c6cc9748a2fa5e40b7ec9488f1c4b8d1fc1f
RostSuslov <suslovros@gmail.com>
2025-04-18 21:21:12 +0300

    feat: added setting to product swiper

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -10,4 +10,34 @@ burger.addEventListener('click', toggleMobileContainer)
 
 const label = document.querySelectorAll('.label')
 const btn = document.querySelectorAll('.btn')
- 
+
+
+const swiper = new Swiper('.swiper-product', {
+    slidesPerView: 4,
+    loop: true,
+    speed: 500,
+    spaceBetween: 48,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // breakpoints: {
+    //     // when window width is >= 320px
+    //     320: {
+    //         slidesPerView: 2,
+    //         spaceBetween: 20
+    //     },
+    //     // when window width is >= 480px
+    //     480: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30
+    //     },
+    //     // when window width is >= 640px
+    //     640: {
+    //         slidesPerView: 4,
+    //         spaceBetween: 40
+    //     }
+    // }
+});
